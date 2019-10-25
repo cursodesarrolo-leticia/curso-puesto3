d91f817dc6a75b9954eda29ebd9dfa544ac09d0d
Sandra <sandri@MacBook-Pro-de-Sandra.local>
2019-10-25 18:24:43 +0200

    prueba git en casa

--- header.php
+++ header.php
@@ -21,7 +21,7 @@
 *  @author PrestaShop SA <contact@prestashop.com>
 *  @copyright  2007-2017 PrestaShop SA
 *  @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
-*  International Registered Trademark & Property of PrestaShop SA
+*  Inte rnational Registered Trademark & Property of PrestaShop SA
 */
 
 if (isset(Context::getContext()->controller)) {
